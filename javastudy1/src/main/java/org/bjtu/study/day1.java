8744f432da0d24d283963b0f715343687d7b15a1
fq <940226637@qq.com>
2020-11-14 00:37:49 +0800

    day1(MyStuArray1)

--- javastudy1/src/main/java/org/bjtu/study/day1.java
+++ javastudy1/src/main/java/org/bjtu/study/day1.java
@@ -1,15 +1,74 @@
 package org.bjtu.study;
 
 import java.util.*;
+import java.math.*;
+
+/**
+ * @author fq
+ * @date 2020.11.13
+ */
+
+class Student{
+    int number;
+    int state;
+    int score;
+
+    public void showInfo(){
+        System.out.format("%d %d %d",this.number,this.state,this.score);
+        System.out.println();
+    }
+
+    Student(int num,int sta,int sco){
+        number = num;
+        state = sta;
+        score = sco;
+    }
+}
 
 public class day1 {
+    /**
+     * @Description search special Student in stu
+     * @date 2020.11.13
+     * @param stu target array
+     */
+    public static void searchStuArray(Student[] stu){
+        for(Student s : stu){
+            if(s.state == 3){
+                s.showInfo();
+            }
+        }
+    }
+
+    public static void printStuArray(Student[] stu){
+        for(Student s : stu){
+            System.out.format("%s %s %s",s.number,s.state,s.score);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr1 = new int[5];
-        for(int i = 0;i < 5;++i){
-            arr1[i] = 2*i;
+        Student[] stu = new Student[20];
+        int size = stu.length;
+        for(int i = 1;i <= 20;++i){
+            Student s = new Student(i,(int)((Math.random()*3)+1),(int)((Math.random()*100)+1));
+            stu[i-1] = s;
         }
-        for(int num : arr1){
-            System.out.println(num);
+
+        searchStuArray(stu);
+
+        //bubblesort
+        for(int i = 0;i < size - 1;++i){
+            for(int j = 0;j < size - i - 1;++j){
+                if(stu[j].score > stu[j + 1].score){
+                    Student temp = stu[j + 1];
+                    stu[j + 1] = stu[j];
+                    stu[j] = temp;
+                }
+            }
         }
+
+        System.out.println("------------------------");
+
+        printStuArray(stu);
     }
 }
