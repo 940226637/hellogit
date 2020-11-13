d69e24dc384b5747f0445b96061db41dc8819015
fq <940226637@qq.com>
2020-11-13 21:27:10 +0800

    day1(2)

--- javastudy1/src/main/java/org/bjtu/study/day1.java
+++ javastudy1/src/main/java/org/bjtu/study/day1.java
@@ -5,7 +5,11 @@ import java.util.*;
 public class day1 {
     public static void main(String[] args) {
         int[] arr1 = new int[5];
-
-
+        for(int i = 0;i < 5;++i){
+            arr1[i] = 2*i;
+        }
+        for(int num : arr1){
+            System.out.println(num);
+        }
     }
 }
